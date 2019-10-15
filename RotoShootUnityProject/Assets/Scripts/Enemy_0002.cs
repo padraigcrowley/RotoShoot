feb27ed8f1b4cecc37ea355687dfd8600f8f618f
padraigcrowley <padraig.crowley@gmail.com>
2019-10-15 23:53:40 +0100

    updated enemy code

--- RotoShootUnityProject/Assets/Scripts/Enemy_0002.cs
+++ RotoShootUnityProject/Assets/Scripts/Enemy_0002.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Enemy_0002 : EnemyBehaviour
 {
-  private float knockBackAmount = 50f;
+  private float knockBackAmount = 100f;
+  
 
-  public override void ReactToPlayerMissileHit(GameObject missile)
+  public override void ReactToPlayerMissileHit()
   {
-    transform.localScale *= 2f; // scale slightly up to show they've been shot
+    transform.localScale *= 1.1f; // scale slightly up to show they've been shot
 
     // Move our position a step closer to the target.
     float step = knockBackAmount * Time.deltaTime; // calculate distance to move
     transform.position = Vector3.MoveTowards(transform.position, GameplayManager.Instance.playerShipPos, -step);
+    
   }
 }
