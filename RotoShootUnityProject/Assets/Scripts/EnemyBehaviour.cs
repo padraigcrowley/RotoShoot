6e82f7ef04b746934a90d6ce066b804f1251bf32
padraig.crowley <padraig.crowley@gmail.com>
2019-10-21 13:36:26 +0100

    further updates to enemybehaviour states

--- RotoShootUnityProject/Assets/Scripts/EnemyBehaviour.cs
+++ RotoShootUnityProject/Assets/Scripts/EnemyBehaviour.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public abstract class EnemyBehaviour : ExtendedBehaviour
 //https://answers.unity.com/questions/379440/a-simple-wait-function-without-coroutine-c.html
@@ -20,8 +17,9 @@ public abstract class EnemyBehaviour : ExtendedBehaviour
 
   private GameObject missileObject;
 
-  private enum EnemyState { ALIVE, TEMPORARILY_DEAD, WAITING_TO_RESPAWN , INVINCIBLE, FULLY_DEAD, HIT_BY_PLAYER_MISSILE }
+  private enum EnemyState { ALIVE, TEMPORARILY_DEAD, WAITING_TO_RESPAWN, INVINCIBLE, FULLY_DEAD, HIT_BY_PLAYER_MISSILE, HIT_BY_PLAYER_SHIP }
   private EnemyState enemyState;
+  private bool respawnWaitOver; 
 
   //----------------------
   public abstract void ReactToNonLethalPlayerMissileHit();
@@ -31,19 +29,20 @@ public abstract class EnemyBehaviour : ExtendedBehaviour
     //todo - move this out to GameplayManager or to sub-class or prefab??
     speed = .25f;
     hp = 1f;
-    initialSpeed = speed * speedMultiplierFromSpawner; // should these be set in this class, not MyGameplayManager??
+    initialSpeed = speed * speedMultiplierFromSpawner; // todo: should these be set in this class, not MyGameplayManager??
     initialHP = hp * hpMultiplierFromSpawner;
     speed = initialSpeed;
     hp = initialHP;
 
     enemyState = EnemyState.ALIVE;
     enemyHitByPlayerMissile = false;
+    respawnWaitOver = false;
 
     // rotate enemy to face player ship https://answers.unity.com/questions/585035/lookat-2d-equivalent-.html
     Vector3 dir = GameplayManager.Instance.playerShipPos - transform.position;
     float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90f;
     transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        
+
     startPosX = transform.position.x;
     startPosY = transform.position.y;
     startPosZ = transform.position.z;
@@ -59,62 +58,59 @@ public abstract class EnemyBehaviour : ExtendedBehaviour
       switch (enemyState)
       {
         case EnemyState.ALIVE:
-        {
+          {
             // Move our position a step closer to the target.
             float step = speed * Time.deltaTime; // calculate distance to move
             transform.position = Vector3.MoveTowards(transform.position, GameplayManager.Instance.playerShipPos, step);
             break;
-        }
+          }
         case EnemyState.TEMPORARILY_DEAD:
-        {
+          {
 
-          break;
-        }
-        case EnemyState.HIT_BY_PLAYER_MISSILE:
-        {
-            Destroy(missileObject);//destroy the missile object - should the missileObject itself be doing this or at least pass a message back to it?
-            if (hp <= 1) //lethal hit
-            {              
-              LevelManager.Instance.numEnemyKillsInLevel++;
-
-              //https://answers.unity.com/questions/379440/a-simple-wait-function-without-coroutine-c.html
-              Wait(5, () => {
-                Debug.Log("5 seconds is lost forever");
-              });
-
-              //if (!readyToRespawn)
-              {
-                hp = initialHP; //reset health and position
-                transform.position = new Vector3(startPosX, startPosY, startPosZ);
-                transform.localScale = new Vector3(startScaleX, startScaleX, startScaleX); // reset its scale back to original scale
-              }
-              enemyState = EnemyState.ALIVE;
-            }
-            else //non-lethal hit
-            {              
-              hp--;
-              ReactToNonLethalPlayerMissileHit();
-            }
             break;
-        }
+          }
+        case EnemyState.HIT_BY_PLAYER_MISSILE:
+          {
+            Destroy(missileObject);//destroy the missile object - should the missileObject itself be doing this or at least pass a message back to it?
+            HandleDamage();
+            
+            break;
+          }
+        case EnemyState.HIT_BY_PLAYER_SHIP:
+          {
+            GameplayManager.Instance.currentPlayerHP--;
+            hp = initialHP; //reset health and position
+            transform.position = new Vector3(startPosX, startPosY, startPosZ);
+            transform.localScale = new Vector3(1f, 1f, 1f); // reset its scale back to 1
+            //TODO: THEN SET STATE TO WHAT??
+            break;
+          }
         case EnemyState.WAITING_TO_RESPAWN:
-        {
-          break;
-        }
+          {
+            //https://answers.unity.com/questions/379440/a-simple-wait-function-without-coroutine-c.html
+            Wait(2, () =>
+            {
+              respawnWaitOver = true;
+              Debug.Log("2 seconds is lost forever");
+            });
+            if (respawnWaitOver)
+              Respawn();
+            break;
+          }
         case EnemyState.INVINCIBLE:
-        {
-          break;
-        }
+          {
+            break;
+          }
         case EnemyState.FULLY_DEAD:
-        {
-          break;
-        }
+          {
+            break;
+          }
         default:
-        {
-          break;
-        }
+          {
+            break;
+          }
       }
-                 
+
     }
     else if (GameplayManager.Instance.currentGameState == GameplayManager.GameState.GAME_OVER_SCREEN)
     {
@@ -123,6 +119,29 @@ public abstract class EnemyBehaviour : ExtendedBehaviour
       transform.position = new Vector3(startPosX, startPosY, startPosZ);
       transform.localScale = new Vector3(startScaleX, startScaleX, startScaleX); // reset its scale  
     }
+  }
+
+  private void HandleDamage()
+  {
+    if (hp <= 1) //lethal hit
+    {
+      LevelManager.Instance.numEnemyKillsInLevel++;
+      enemyState = EnemyState.WAITING_TO_RESPAWN;
+    }
+    else //non-lethal hit
+    {
+      hp--;
+      ReactToNonLethalPlayerMissileHit();
+      enemyState = EnemyState.ALIVE;
+    }
+  }
+
+  private void Respawn()
+  {
+      hp = initialHP; //reset health and position
+      transform.position = new Vector3(startPosX, startPosY, startPosZ);
+      transform.localScale = new Vector3(startScaleX, startScaleX, startScaleX); // reset its scale back to original scale    
+      enemyState = EnemyState.ALIVE;
   }
 
   private void OnTriggerEnter2D(Collider2D collision)
@@ -136,14 +155,11 @@ public abstract class EnemyBehaviour : ExtendedBehaviour
       }
       else if (collision.gameObject.tag.Equals("Player"))
       {
-        GameplayManager.Instance.currentPlayerHP--;
-        hp = initialHP; //reset health and position
-        transform.position = new Vector3(startPosX, startPosY, startPosZ);
-        transform.localScale = new Vector3(1f, 1f, 1f); // reset its scale back to 1
+        enemyState = EnemyState.HIT_BY_PLAYER_SHIP;
       }
     }
   }
 
-  
-  
+
+
 }
--- RotoShootUnityProject/Assets/Scripts/Enemy_0002.cs
+++ RotoShootUnityProject/Assets/Scripts/Enemy_0002.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Enemy_0002 : EnemyBehaviour
 {
   private float knockBackAmount = 99f;
-  
+
 
   public override void ReactToNonLethalPlayerMissileHit()
   {
@@ -14,6 +12,6 @@ public class Enemy_0002 : EnemyBehaviour
     // Move our position a step closer to the target.
     float step = knockBackAmount * Time.deltaTime; // calculate distance to move
     transform.position = Vector3.MoveTowards(transform.position, GameplayManager.Instance.playerShipPos, -step);
-    
+
   }
 }
