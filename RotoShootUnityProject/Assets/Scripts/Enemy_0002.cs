a4d61c65526212ad16431b48723fcfff59061e98
padraig.crowley <padraig.crowley@gmail.com>
2019-10-25 09:26:48 +0100

    Revert "another.."
    This reverts commit 02e659ab1699522e174d2ea596f3a474f1e20d6a.

--- RotoShootUnityProject/Assets/Scripts/Enemy_0002.cs
+++ RotoShootUnityProject/Assets/Scripts/Enemy_0002.cs
@@ -14,7 +14,7 @@ public class Enemy_0002 : EnemyBehaviour
   {
     transform.localScale *= 1.1f; // scale slightly up to show they've been shot
 
-    // knock the enemy back
+    // knock back the enemy
     float step = knockBackAmount * Time.deltaTime; // calculate distance to move
     transform.position = Vector3.MoveTowards(transform.position, GameplayManager.Instance.playerShipPos, -step);
   }
