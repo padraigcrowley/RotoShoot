76b027cf70a683dfacdda0a3abbff9cb8e78c192
padraigcrowley <padraig.crowley@gmail.com>
2020-01-20 12:34:25 +0000

    updates

---
+++ RotoShootUnityProject/Assets/TestUniqueProjectiles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUniqueProjectiles : MonoBehaviour
+{
+  // Start is called before the first frame update
+  public GameObject effectToSpawn;
+  
+
+  void Start()
+    {
+      GameObject vfx;
+      //vfx = Instantiate(effectToSpawn);
+    vfx = Instantiate(effectToSpawn, new Vector3(0f,0f,0f), Quaternion.identity);
+    //vfx.transform.localRotation = ;
+
+  }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
