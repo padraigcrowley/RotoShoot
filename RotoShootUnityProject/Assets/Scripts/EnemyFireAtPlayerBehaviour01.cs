7bc82e2b0eaf2f27165a97a0faa0650f1f4ea46f
padraigcrowley <padraig.crowley@gmail.com>
2020-03-02 12:33:47 +0000

    Update EnemyFireAtPlayerBehaviour01.cs

--- RotoShootUnityProject/Assets/Scripts/EnemyFireAtPlayerBehaviour01.cs
+++ RotoShootUnityProject/Assets/Scripts/EnemyFireAtPlayerBehaviour01.cs
@@ -7,21 +7,21 @@ public class EnemyFireAtPlayerBehaviour01 : MonoBehaviour
   [SerializeField] private GameObject enemyMissile;
 
   void Start()
-    {
+  {
     InvokeRepeating("FireMissileAtPlayerPos", 3, 5);
   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 
   private void FireMissileAtPlayerPos()
   {
     GameObject firedBullet;
     //Dont shoot if the y pos is almost same as playership
-    if (transform.position.y-3 > GameplayManager.Instance.playerShipPos.y)
+    if (transform.position.y - 3 > GameplayManager.Instance.playerShipPos.y)
     {
       firedBullet = Instantiate(enemyMissile, transform.position, transform.rotation);
       Debug.Log("FireMissileAtPlayerPos()");
