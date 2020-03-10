47a3b179a40758af3ac3e3236674069cc11a6bf8
padraigcrowley <padraig.crowley@gmail.com>
2020-03-10 09:57:51 +0000

    muzzleflashvfx test scene

--- RotoShootUnityProject/Assets/projectile_Test.cs
+++ RotoShootUnityProject/Assets/projectile_Test.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class projectile_Test : MonoBehaviour
+public class projectile_Test : ExtendedBehaviour
 {
-    // Start is called before the first frame update
+  // Start is called before the first frame update
+  public GameObject muzzleVFX;
+
+
     void Start()
     {
-        
-    }
+
+    
+
+  }
 
     // Update is called once per frame
     void Update()
     {
-        
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      //psChild.Stop();
+      print($"muzzleVFX.SetActive(false);");
+      Instantiate(muzzleVFX, transform.position, transform.rotation);
+
     }
+  }
 }
