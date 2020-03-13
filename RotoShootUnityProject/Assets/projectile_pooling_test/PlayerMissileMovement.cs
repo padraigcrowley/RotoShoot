8da3d8a7ffce8524fb47365d66d20f6be107c1c9
padraig.crowley <padraig.crowley@gmail.com>
2020-03-13 18:34:06 +0000

    Update PlayerMissileMovement.cs

--- RotoShootUnityProject/Assets/projectile_pooling_test/PlayerMissileMovement.cs
+++ RotoShootUnityProject/Assets/projectile_pooling_test/PlayerMissileMovement.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class PlayerMissileMovement : MonoBehaviour
+public class PlayerMissileMovement : ExtendedBehaviour
 {
   private Vector3 upDirection;
   public float speed = 5;
@@ -11,7 +12,30 @@ public class PlayerMissileMovement : MonoBehaviour
   void Start()
   {
     upDirection = GameObject.FindGameObjectWithTag("Player").transform.up;
+    EditorApplication.isPaused = true;
     SimplePool.Spawn(vfxMuzzleFlash, transform.position, transform.rotation);
+
+    if (vfxMuzzleFlash != null)
+    {
+      var muzzleVFX = SimplePool.Spawn(vfxMuzzleFlash, transform.position, Quaternion.identity);
+      muzzleVFX.transform.forward = gameObject.transform.forward;// + offset;
+      var ps = muzzleVFX.GetComponent<ParticleSystem>();
+      if (ps != null)
+      {
+        Wait(ps.main.duration, () => {
+          SimplePool.Despawn(muzzleVFX);
+        });
+      }
+
+      else
+      {
+        var psChild = muzzleVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+        Wait(.3f, () => {
+          print($"Waited {psChild.main.duration} before Despawn");
+          SimplePool.Despawn(muzzleVFX);
+        });
+      }
+    }
   }
 
   void FixedUpdate()
