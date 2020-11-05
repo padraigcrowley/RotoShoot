0aa67a8f5136158102528e1b9bbdd9dbd5d61ded
padraigcrowley <padraig.crowley@gmail.com>
2020-11-05 20:19:54 +0000

    added atmos BG

---
+++ RotoShootUnityProject/Assets/SpriteAnimTest/ExplosionGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionGenerator : MonoBehaviour
+{
+  public Animator boom;  
+  // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown("x"))
+    {
+      print("BOOM!");
+      boom.Play("fireexplosion");
+    }
+  }
+}
