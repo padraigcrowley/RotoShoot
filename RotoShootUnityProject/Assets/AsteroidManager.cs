d121fa07cd1892c0723583049ccd91c0bd92387c
padraigcrowley <padraig.crowley@gmail.com>
2020-11-13 00:23:10 +0000

    moar astriods!

--- RotoShootUnityProject/Assets/AsteroidManager.cs
+++ RotoShootUnityProject/Assets/AsteroidManager.cs
@@ -5,12 +5,14 @@ public class AsteroidManager : MonoBehaviour
 {
   private GameObject asteroids1Instance, asteroids2Instance;
   public GameObject asteroids1Prefab, asteroids2Prefab;
-  private float travelSpeed = 0.5f;
+  private float travelSpeed = .1f;
 
   private bool asteroidsCreated = false;
   private List<GameObject> asteroidChildrenObjects = new List<GameObject>();
-  private Animator asteroids1RotationAnimator;
-  private SpriteRenderer asteroidSprite;
+  private Animator asteroids1RotationAnimator, asteroids2RotationAnimator;
+  private SpriteRenderer asteroid1Sprite, asteroid2Sprite;
+
+  private float timeBetweenAsteroidShower = 30f;
 
   private void Start()
   {
@@ -38,8 +40,21 @@ public class AsteroidManager : MonoBehaviour
   {
     for (int i = 0; i < 4; i++)
     {
-      Vector2 pos = new Vector2(1.28f,i*2);
-      asteroids1Instance = SimplePool.Spawn(asteroids1Prefab, pos, transform.rotation, transform);
+      Vector2 pos = new Vector2(1.28f, 4.0f + (i*2));
+      
+      int rnd = UnityEngine.Random.Range(0, 2) ;
+      print($"RND was {rnd}");
+
+      if (rnd ==0)
+      {
+        
+        asteroids1Instance = SimplePool.Spawn(asteroids1Prefab, pos, transform.rotation, transform);
+      }
+      else
+      {
+        
+        asteroids1Instance = SimplePool.Spawn(asteroids2Prefab, pos, transform.rotation, transform);
+      }
       asteroidChildrenObjects.Add(asteroids1Instance);
     }
     asteroidsCreated = true;
@@ -53,16 +68,19 @@ public class AsteroidManager : MonoBehaviour
 
       asteroids1RotationAnimator = childObj.GetComponent<Animator>();
       asteroids1RotationAnimator.Play("asteroids_1", -1, Random.Range(0f, 1f));
-      asteroidSprite = childObj.GetComponent<SpriteRenderer>();
+      asteroids1RotationAnimator.Play("asteroids_2", -1, Random.Range(0f, 1f)); //awful hack!!
+
+      asteroid1Sprite = childObj.GetComponent<SpriteRenderer>();
       if (i == 1)
-        asteroidSprite.flipX = true;
+        asteroid1Sprite.flipX = true;
       if (i == 2)
-        asteroidSprite.flipY = true;
+        asteroid1Sprite.flipY = true;
       if (i == 3)
       {
-        asteroidSprite.flipX = true;
-        asteroidSprite.flipY = true;
+        asteroid1Sprite.flipX = true;
+        asteroid1Sprite.flipY = true;
       }
+      asteroid1Sprite.transform.localScale *= Random.Range(.5f, 1.5f);
       i++;
     }
   }
