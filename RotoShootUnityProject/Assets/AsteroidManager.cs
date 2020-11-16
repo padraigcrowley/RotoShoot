5905f011a8cdd1b4116865b28d3ce8b89ee290c5
padraigcrowley <padraig.crowley@gmail.com>
2020-11-16 18:08:27 +0000

    re-implemented enemy2_000 and some asteroids stuff

--- RotoShootUnityProject/Assets/AsteroidManager.cs
+++ RotoShootUnityProject/Assets/AsteroidManager.cs
@@ -5,7 +5,7 @@ public class AsteroidManager : MonoBehaviour
 {
   private GameObject asteroids1Instance, asteroids2Instance;
   public GameObject asteroids1Prefab, asteroids2Prefab;
-  private float travelSpeed = .1f;
+  private float travelSpeed = .5f;
 
   private bool asteroidsCreated = false;
   private List<GameObject> asteroidChildrenObjects = new List<GameObject>();
@@ -47,7 +47,7 @@ public class AsteroidManager : MonoBehaviour
 
       //if (rnd == 0)
       {
-        asteroids1Instance = SimplePool.Spawn(asteroids2Prefab, pos, transform.rotation, transform);
+        asteroids1Instance = SimplePool.Spawn(asteroids1Prefab, pos, transform.rotation, transform);
       }
       //else
       {
@@ -63,23 +63,33 @@ public class AsteroidManager : MonoBehaviour
     int i = 0;
     foreach (GameObject childObj in asteroidChildrenObjects)
     {
-
+      asteroids1RotationAnimator = childObj.GetComponent<Animator>();
       asteroid1Sprite = childObj.GetComponent<SpriteRenderer>();
+      asteroid1Sprite.transform.localScale *= Random.Range(.25f, 1.5f);
+      
+      if (i == 0)
+      {
+        asteroids1RotationAnimator.speed = .5f ;
+      }
       if (i == 1)
+      { 
         asteroid1Sprite.flipX = true;
+      asteroids1RotationAnimator.speed = .75f;
+
+      }
       if (i == 2)
+      { 
         asteroid1Sprite.flipY = true;
+        asteroids1RotationAnimator.speed = 1f;
+      }
       if (i == 3)
       {
         asteroid1Sprite.flipX = true;
         asteroid1Sprite.flipY = true;
+        asteroids1RotationAnimator.speed = 1.5f;
       }
-      asteroid1Sprite.transform.localScale *= Random.Range(.5f, 1.5f);
-      
-      asteroids1RotationAnimator = childObj.GetComponent<Animator>();
       //asteroids1RotationAnimator.Play("asteroids_1", -1, Random.Range(0f, 1f));
-      asteroids1RotationAnimator.speed = 1+i;
-      asteroids1RotationAnimator.Play("asteroids_2", -1, Random.Range(0f, 1f)); //awful hack!!
+      asteroids1RotationAnimator.Play("asteroids_1", -1, Random.Range(0f, 1f)); //awful hack!!
 
       i++;
     }
--- RotoShootUnityProject/Assets/Scripts/Enemy02_0000.cs
+++ RotoShootUnityProject/Assets/Scripts/Enemy02_0000.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+
 
 /// <summary>
 /// Enemy type that flys into centre of screen (or wherever playership is), towards player ship
@@ -11,16 +13,25 @@ public class Enemy02_0000 : Mr1.EnemyBehaviour02
   protected override void Start()
   {
     base.Start();
-    Vector3 dir = GameplayManager.Instance.playerShipPos - transform.position;
-    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90f;
-    transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    
+    
 
   }
   public override void DoMovement(float initialSpeed, Mr1.FollowType followType)
   {
     
     float step = speed * Time.deltaTime; // calculate distance to move
-    transform.position = Vector3.MoveTowards(transform.position, GameplayManager.Instance.playerShipPos, step);
+    if ((transform.position.y - 3.1) > GameplayManager.Instance.playerShipPos.y)
+      transform.position = Vector3.MoveTowards(transform.position, new Vector2(GameplayManager.Instance.playerShipPos.x, GameplayManager.Instance.playerShipPos.y + 3.1f), step);
+    else
+      transform.position = Vector3.MoveTowards(transform.position, GameplayManager.Instance.playerShipPos, step);
+
+    // rotate to aim at playership
+    Vector3 dir = GameplayManager.Instance.playerShipPos - transform.position;
+    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90f;
+    //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    transform.DORotateQuaternion(Quaternion.AngleAxis(angle, Vector3.forward),1);
+
 
   }
 
