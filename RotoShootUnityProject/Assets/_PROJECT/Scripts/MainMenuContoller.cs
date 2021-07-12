f839a431dad6c5d016637529e7c04b9a3f354f3e
padraigcrowley <padraig.crowley@gmail.com>
2021-07-12 12:13:26 +0100

    misc menu stuff

--- RotoShootUnityProject/Assets/_PROJECT/Scripts/MainMenuContoller.cs
+++ RotoShootUnityProject/Assets/_PROJECT/Scripts/MainMenuContoller.cs
@@ -3,11 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BeautifulTransitions.Scripts.Transitions;
+using BeautifulTransitions.Scripts.Transitions.TransitionSteps;
 
 public class MainMenuContoller : MonoBehaviour
 {
   public GameObject MainMenuButtonTransitions;
   public GameObject LogoImageTransitions;
+  public GameObject logoGameObject;
 
 
   // Start is called before the first frame update
@@ -19,7 +21,15 @@ public class MainMenuContoller : MonoBehaviour
   public void MainMenuStartButtonTransitionOut()
   {
     TransitionHelper.TransitionOut(MainMenuButtonTransitions);
-    TransitionHelper.TransitionIn(LogoImageTransitions);
+    //TransitionHelper.TransitionIn(LogoImageTransitions);
+  }
+  public void MainMenuUpgradesButtonTransitionOut()
+  {
+    var logoTransitionOut = new Move(logoGameObject, endPosition:new Vector3(0,1520,0), duration:.6f, tweenType: TransitionHelper.TweenType.easeInBack, coordinateSpace: BeautifulTransitions.Scripts.Transitions.TransitionSteps.AbstractClasses.TransitionStep.CoordinateSpaceType.AnchoredPosition );
+    logoTransitionOut.Start();
+
+    TransitionHelper.TransitionOut(MainMenuButtonTransitions);
+    //TransitionHelper.TransitionIn(LogoImageTransitions);
   }
 
   // Update is called once per frame
