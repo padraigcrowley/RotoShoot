41b89fc6e87c2f5ea3dff11d96cbdfd0df6d4ab2
padraigcrowley <padraig.crowley@gmail.com>
2021-07-13 17:18:05 +0100

    mmenu stuffz

--- RotoShootUnityProject/Assets/_PROJECT/Scripts/MainMenuContoller.cs
+++ RotoShootUnityProject/Assets/_PROJECT/Scripts/MainMenuContoller.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using BeautifulTransitions.Scripts.Transitions;
 using BeautifulTransitions.Scripts.Transitions.TransitionSteps;
 
@@ -12,6 +13,9 @@ public class MainMenuContoller : MonoBehaviour
   public GameObject LogoImageTransitions;
   public GameObject logoGameObject;
 
+  public Image logoImage;
+  private Material logoImageMaterial;
+  
 
   void Start()
   {
@@ -20,10 +24,14 @@ public class MainMenuContoller : MonoBehaviour
 
     TransitionHelper.TransitionIn(MainMenuButtonTransitions);
 		TransitionHelper.TransitionIn(LogoImageTransitions);
+
+    logoImageMaterial = logoImage.material;
+
 	}
   public void MainMenuStartButtonTransitionOut()
   {
     TransitionHelper.TransitionOut(MainMenuButtonTransitions);
+    
   }
 
   public void DoLogoTransitionOut()
@@ -41,6 +49,45 @@ public class MainMenuContoller : MonoBehaviour
   {
     DoLogoTransitionOut();
     TransitionHelper.TransitionOut(MainMenuButtonTransitions);
+  }
+
+  public void DoLogoShine() // a separate method because calling this from the OnTransitionComplete event on the logoimage's TransitionOut component.
+	{
+    StartCoroutine(LogoShine());
+  }
+
+  IEnumerator LogoShine(float duration = .5f, float interval = 5f, float delay = 0f)
+	{
+    float elapsedTime = 0f;
+    float currentVal;
+    if (delay > 0f) yield return new WaitForSeconds(delay);
+
+    var waithandle = new WaitForSeconds(interval);
+    while (true)  //https://forum.unity.com/threads/invokerepeating-or-coroutine.875965/
+    {
+      elapsedTime = 0f;
+      while (elapsedTime <= duration) //from normal to red
+      {
+        //sr.material.SetFloat("_ChromAberrAmount", 0f);
+        currentVal = Mathf.Lerp(0f, 1f, (elapsedTime / duration));
+        logoImageMaterial.SetFloat("_ShineLocation", currentVal);
+        elapsedTime += Time.deltaTime;
+
+        //yield return null;
+        yield return new WaitForEndOfFrame();
+      }
+      elapsedTime = 0f;
+      while (elapsedTime <= duration) //from normal to red
+      {
+        //sr.material.SetFloat("_ChromAberrAmount", 0f);
+        currentVal = Mathf.Lerp(1f, 0f, (elapsedTime / duration));
+        logoImageMaterial.SetFloat("_ShineLocation", currentVal);
+        elapsedTime += Time.deltaTime;
+
+        //yield return null;
+        yield return new WaitForEndOfFrame();
+      }
+    }
   }
 
   void Update()
