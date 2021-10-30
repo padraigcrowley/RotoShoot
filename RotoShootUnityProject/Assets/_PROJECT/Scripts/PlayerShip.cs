46a9aad698c805184e36d33daff3da9a1bb8e2d2
padraigcrowley <padraig.crowley@gmail.com>
2021-10-30 22:15:46 +0100

    etczz

--- RotoShootUnityProject/Assets/_PROJECT/Scripts/PlayerShip.cs
+++ RotoShootUnityProject/Assets/_PROJECT/Scripts/PlayerShip.cs
@@ -377,7 +377,7 @@ public class PlayerShip : ExtendedBehaviour
     {
       if (GameplayManager.Instance.currentGameState == GameplayManager.GameState.LEVEL_IN_PROGRESS)
       {
-        if ((collision.gameObject.tag.Equals("Enemy01")) || (collision.gameObject.tag.Equals("EnemyMissile")) || (collision.gameObject.tag.Equals("EnemyMissileLarge")))
+        if ((collision.gameObject.tag.Equals("Enemy01")) || (collision.gameObject.tag.Equals("EnemyMissile")) || (collision.gameObject.tag.Equals("EnemyMissileMedium")) || (collision.gameObject.tag.Equals("EnemyMissileLarge")))
         {
           //GameObject newParticleEffect = GameObject.Instantiate(PlayerShipDamageLarge, transform.position, PlayerShipDamageLarge.transform.rotation, transform ) as GameObject;
           GameObject newParticleEffect = SimplePool.Spawn(PlayerShipDamageLarge, collision.gameObject.transform.position, PlayerShipDamageLarge.transform.rotation, transform) as GameObject;
@@ -391,11 +391,11 @@ public class PlayerShip : ExtendedBehaviour
           //print($"collision between this {transform.position} and other {collision.gameObject.transform.position}");
           DoCameraShake();
           if ((collision.gameObject.tag.Equals("Enemy01")) || (collision.gameObject.tag.Equals("EnemyMissile")))
-            ChangeShipHP((int)LevelManager.Instance.LevelStats["EnemyMissileDamage"]); 
+            ChangeShipHP(-(int)LevelManager.Instance.LevelStats["EnemyMissileDamage"]); 
           else if (collision.gameObject.tag.Equals("EnemyMissileLarge"))
-            ChangeShipHP((int)LevelManager.Instance.LevelStats["EnemyMissileDamage"]*3); 
+            ChangeShipHP(-(int)LevelManager.Instance.LevelStats["EnemyMissileDamage"]*10); 
           else if (collision.gameObject.tag.Equals("EnemyMissileMedium"))
-            ChangeShipHP((int)LevelManager.Instance.LevelStats["EnemyMissileDamage"]*10); 
+            ChangeShipHP(-(int)LevelManager.Instance.LevelStats["EnemyMissileDamage"]*3); 
 
 
         }
